51bc1ce3f39928bead8421a67dc57c71d97bf4be
Rayhub <1102290321@qq.com>
2024-01-09 12:51:01 +0800

    update sniff script

--- diane/src/sniffer/dump_to_pcap.sh
+++ diane/src/sniffer/dump_to_pcap.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec sshpass -p $1 ssh $4@$2 'tcpdump -i '$5' -s0 -w - not port 22' > $3 2>/dev/null
+exec sshpass -p $1 ssh $4@$2 'echo '$1' | sudo -S tcpdump -i '$5' -s0 -w - not port 22' > $3 2>/dev/null
--- diane/src/sniffer/sniff.sh
+++ diane/src/sniffer/sniff.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec sshpass -p $1 ssh $5@$2 'tcpdump -e -i '$6' -l not port 22 and src host' $3 'and dst host '$4 `shift 6; echo "${@}"` > /tmp/sniff_data 2>/dev/null
+exec sshpass -p $1 ssh $5@$2 'echo '$1' | sudo -S tcpdump -e -i '$6' -l not port 22 and src host' $3 'and dst host '$4 `shift 6; echo "${@}"` > /tmp/sniff_data 2>/dev/null
